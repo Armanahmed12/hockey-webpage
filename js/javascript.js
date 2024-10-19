e598526ad90bbc14da8d2651e4dc8269658a3630
Armanahmed12 <mdarmanahmed1232@gmail.com>
2024-10-19 19:41:05 +0600

    some changes has been done

--- js/javascript.js
+++ js/javascript.js
@@ -1,33 +1,26 @@
+const ulTagOfNav = document.getElementById("nav-lists-container");
+const menuBar = document.getElementById("menuBarr");
+const bannerCarouselImgContainer = document.getElementById(
+  "bannerCarouselSection"
+);
+// navigation bar
+document.getElementById("menuBar").addEventListener("click", () => {
+  bannerCarouselImgContainer.style.zIndex = -20;
 
-const ulTagOfNav = document.getElementById('nav-lists-container');
-const menuBar = document.getElementById('#menuBarr');
+  const styles = [...ulTagOfNav.classList];
 
-// navigation bar 
-document.getElementById('menuBar').addEventListener('click', ()=>{
-
-     const styles = [...ulTagOfNav.classList];
-
-           styles.forEach(element => {
-        //    console.log(element);
-            if(element.startsWith('top-')){
-
-                if(element !== 'top-[80px]'){
-
-                    ulTagOfNav.classList.remove('top-[-600px]');
-                    ulTagOfNav.classList.add('top-[80px]');
-                    
-                    
-               }
-              else{
-               
-                ulTagOfNav.classList.remove('top-[80px]');
-                ulTagOfNav.classList.add('top-[-600px]');
-              
-    
-                    
-              }
-                 
-            }
-      });
-})
-
+  styles.forEach((element) => {
+    if (element.startsWith("top-")) {
+      if (element !== "top-[80px]") {
+        ulTagOfNav.classList.remove("top-[-600px]");
+        ulTagOfNav.classList.add("top-[80px]");
+      } else {
+        ulTagOfNav.classList.remove("top-[80px]");
+        ulTagOfNav.classList.add("top-[-600px]");
+        setTimeout(() => {
+          bannerCarouselImgContainer.removeAttribute("style");
+        }, 1000);
+      }
+    }
+  });
+});
